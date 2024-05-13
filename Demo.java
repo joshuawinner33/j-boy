634ca0cb5268e16d64cccb416454053023fb751c
joshuawinner414@gmail.com <joshuawinner414@gmail.com>
2024-05-13 05:22:24 -0700

    wizzy

---
+++ Demo.java
@@ -0,0 +1,40 @@
+public class Demo{
+    public static void main(String[] args){
+      //  int x = 6;
+      //  int y = 5;
+       // int b = 5;
+       // int c = 9;
+      //  boolean result = x<y;
+      //boolean result = x>y && b>c;
+     // boolean result = x>y || b>c;
+     //   System.out.println(result);
+     // if
+    // int x = 8;
+    // int y = 7;
+   // if (x>y)
+   //if (x%2==0)
+   //if (x%3==2)
+  // if (y%2==0)
+   // System.out.println("yes");
+  //  else
+  //  System.out.println("no");
+   //else if
+  // int x = 8;
+  // int y = 7;
+  // int z = 9;
+  // if (x < y && y < z)
+  // System.out.println("True");
+  // else if (x > z || y > z)
+  // System.out.println ("false");
+  // else
+  // System.out.print ("joshua");
+   int n =4;
+   int result = 0;
+   //if (n%2==0)
+  // result += 10;
+   //else result += 20;
+   //result = n%2!=0? 10:20;
+   result = n%2==0? 10:20;
+   System.out.println(result);
+    }
+  }
---
+++ Tedi.java
@@ -0,0 +1,23 @@
+public class Tedi{
+    public static void main(String[] args){
+     // int num_1 = 256;
+     // byte by = 127;
+     // short sh = 558;
+     //long lg = 5854l;
+    // double db = 5.8;
+    // float ft = 5.8f;
+    //num_1 = by;
+    //byte by = 10;
+   // byte bys = 30;
+   // int result = by*bys;
+  int num = 7;
+   num = num+2;
+   num++;
+  ++num;
+ num = num-1;
+ num--;
+ num = num*2;
+ num = num*num;
+        System.out.println(num);
+    }
+}
